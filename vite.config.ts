f9c398a0b740c88b5be841e42d5bada5ac97ef6b
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-01-22 13:01:39 +0100

    fix: puslish lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["src"] })],
   build: {
-    outDir: "dist",
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+    },
   },
 });
